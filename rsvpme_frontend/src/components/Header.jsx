86b42afe4facba9b58c6ed4960d50ed2b4461bb0
benjamin-le-roux <blerouxj@gmail.com>
2025-02-14 16:02:34 +0200

    Creating REACT Frontend

---
+++ rsvpme_frontend/src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+function Header() {
+    return (
+        <div>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <button>button 1</button>
+                <button>button 2</button>
+                <button>button 3</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
