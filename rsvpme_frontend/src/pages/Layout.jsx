ef52636317c57c030dfd9cc9a6f27e0742f8345f
benjamin-le-roux <blerouxj@gmail.com>
2025-03-10 09:52:39 +0200

    Minor frontend progression

--- rsvpme_frontend/src/pages/Layout.jsx
+++ rsvpme_frontend/src/pages/Layout.jsx
@@ -3,15 +3,14 @@ import './../css/Layout.css'
 
 const Layout = () => (
     <>
-        {/* <img src="/src/resources/bg.jpg" class="bg-img" /> */}
         <div className='nav-bar-container'>
                 <div className='nav-bar'>
                     <div>
                         <h1>RSVPME</h1>
                     </div>
                     <div>
-                        <Link className='nav-button' to="/">Home</ Link>
-                        <Link className='nav-button' to="/info">Info</ Link>
+                        <Link className='nav-button' to="/">HOME</ Link>
+                        <Link className='nav-button' to="/info">RSVP</ Link>
                     </div>
                 </div>
             </div>
